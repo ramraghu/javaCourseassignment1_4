73d79e0c760bd33dd4ede7d77adb4200dd97b378
ramraghu <raghuramnair2015@gmail.com>
2016-09-26 02:05:04 +0530

    firstcommit

---
+++ assignment1_4.java
@@ -0,0 +1,15 @@
+package learnJava;
+
+public class assignment1_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Write a program to print the characters corresponding to ASCII codes from 65 to 90.
+		int i;
+		for (i = 65 ; i < 91; ++i) {
+			System.out.println((char) i);
+			
+		}
+	}
+
+}
